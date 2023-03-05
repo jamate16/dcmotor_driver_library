fdbdca28c31e5700f46cf607c1d09d37f7e72315
jamate <mateoarcilalopez@gmail.com>
2023-03-04 22:31:02 -0500

    Maximizes frequency accuracy and dc resolution

--- Driver.cpp
+++ Driver.cpp
@@ -1,11 +1,15 @@
 #include "Driver.h"
 
-DCMotorDriver::DCMotorDriver(uint pinA1, uint pinA2, float _pwm_freq) {
+DCMotorDriver::DCMotorDriver(uint pinA1, uint pinA2, float _pwm_freq, bool _pwm_phase_correct) {
     PWMGPIOs[0].pin = pinA1;
     PWMGPIOs[1].pin = pinA2;
 
     pwm_freq = _pwm_freq;
+    pwm_phase_correct = _pwm_phase_correct;
+
+    pwm_dc = 0;
 }
+
 
 void DCMotorDriver::init() {
     setPWMsGPIO();
@@ -23,17 +27,60 @@ void DCMotorDriver::setPWMsGPIO() {
 }
 
 void DCMotorDriver::setPWMsFreq() {
-    uint16_t wrap = 0;
+    calculatePWMsClockTopAndDiv();
 
-    clock_get_hz()
-    pwm_set_clkdiv_int_frac();
+    // Configure a config struct
+    pwm_config PWMs_config = pwm_get_default_config();
+    pwm_config_set_phase_correct(&PWMs_config, pwm_phase_correct);
+    pwm_config_set_clkdiv_int_frac(&PWMs_config, PWMDiv.int_, PWMDiv.fract);
+    pwm_config_set_wrap(&PWMs_config, pwm_top);
 
+    // Initialize PWM's slices with PWMs_config
     for (auto &gpio: PWMGPIOs) {
-        pwm_config my_config = pwm_get_default_config();
-        pwm_config_set_phase_correct(&my_config, true);
-        pwm_config_set_wrap(&my_config, 10000);
-        pwm_init(gpio.slice, &my_config, true); // If the last argument is false, I could do pwm_set_mask_enabled and they'd be synchronized
+        pwm_init(gpio.slice, &PWMs_config, true); // If the last argument were false, the syncPWMsSlices function would only need to do pwm_set_mask_enabled()
+        pwm_set_chan_level(gpio.slice, gpio.channel, pwm_dc);
     }
+}
+
+// Iterates over values of PWM top(wrap pint) searching for the first clock divider value that satisfies the desired frequency
+void DCMotorDriver::calculatePWMsClockTopAndDiv() {
+    uint32_t sys_clock_freq = clock_get_hz(clk_sys);
+
+    /// \todo Implement guard clauses forr avoiding innecesary calculations based on MIN and MAX frequencies
+    const float MIN_FREQ = static_cast<float>(sys_clock_freq) / ((TOP_MAX + 1) * (pwm_phase_correct + 1) * (DIV_INT_MAX + DIV_FRAC_MAX/16));
+    const float MAX_FREQ = static_cast<float>(sys_clock_freq) / ((1 + 1) * (pwm_phase_correct + 1) * (DIV_INT_MIN));
+
+    pwm_top = TOP_MAX; // Start with the max resolution for the PWMs' DC
+    uint16_t top_decrement = 1; // To make the loop quicker, make this number bigger at the cost of frequency and DC inaccuracy.
+
+    // Decrement the resolution of the PWM's DC until the desired frequency is achieved
+    while (true) {
+        if (pwm_top >= top_decrement) {
+            calculateClockDiv(sys_clock_freq);
+            if (!(PWMDiv.int_ < DIV_INT_MIN)) break; // Frecuency achieved with the highest DC resolution possible
+            pwm_top -= top_decrement;
+        } else {
+            // std::cout << "\nCan't achieve that desired frequency" << std::endl;
+            break;
+        }
+    }
+}
+
+void DCMotorDriver::calculateClockDiv(uint32_t sys_clock_freq) {
+    // Calculate the value for the divider
+    float div_float = sys_clock_freq / ((pwm_top + 1) * (pwm_phase_correct + 1) * pwm_freq);
+    
+    PWMDiv.int_only = static_cast<uint8_t>(div_float + 0.5);
+
+    // Split the divider's integal part and decimal part into an 8 bit integer and a 4 bit integer respectively
+    PWMDiv.int_ = static_cast<uint8_t>(div_float);
+    uint8_t temp_div_frac = static_cast<uint8_t>((div_float - static_cast<int>(div_float))*16 + 0.5);
+    PWMDiv.fract = (temp_div_frac <= DIV_FRAC_MAX) ? temp_div_frac: DIV_FRAC_MAX; // Max accuracy is 15/16 with this 8.4 clock divider
+
+    // std::cout << "Div float: " << div_float << " number of posible DCs of: " << pwm_top+1 << '\n'; 
+    // Frecuency values for comparison between not using the fract part of the divider and using it
+    // float pwm_freq_real_div_int = (float)sys_clock_freq / ((top + 1) * (phase_correct + 1) * div.int_only); // Only with int part (fract rounded to closest int)
+    // float pwm_freq_real_div_frac = (float)sys_clock_freq / ((top + 1) * (phase_correct + 1) * (div.int_ + div.fract/16.0)); // With int and fract
 }
 
 void DCMotorDriver::syncPWMsSlices() {
@@ -47,6 +94,26 @@ void DCMotorDriver::syncPWMsSlices() {
         // Turn corresponding bit to 1
         driver_gpio_slices_mask |= (1u << gpio.slice);
 	}
-    // Sync the driver's PWM slices taking into account the slices that were enabled before this function's call
+    // Enable the driver's PWM slices together with the slices that were enabled before this function's call
 	pwm_set_mask_enabled(driver_gpio_slices_mask | (pwm_hw->en));
 }
+
+void DCMotorDriver::setPWMsDC(PWMDir dir, float dc) {
+    // Saves the values to the object's attributes just in case they are needed
+    pwm_dc = dc;
+    pwm_dir = dir;
+
+    /// \todo Create a variable that allows to set min and max value for each channel
+    uint16_t pwm_level_sp = static_cast<uint16_t>(dc * pwm_top / 100 + 0.5); // Rounds the scaled dc float to int, casting the value will truncate. Also, dc is always positive
+    switch (dir) {
+        case PWMDir::cw:
+            pwm_set_chan_level(PWMGPIOs[0].slice, PWMGPIOs[0].channel, pwm_level_sp);
+            pwm_set_chan_level(PWMGPIOs[1].slice, PWMGPIOs[1].channel, 0);
+            break;
+        case PWMDir::ccw:
+            pwm_set_chan_level(PWMGPIOs[0].slice, PWMGPIOs[0].channel, 0);
+            pwm_set_chan_level(PWMGPIOs[1].slice, PWMGPIOs[1].channel, pwm_level_sp);
+            break;
+    }
+}
+
--- Driver.h
+++ Driver.h
@@ -4,26 +4,61 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/pwm.h"
+#include "hardware/clocks.h"
 
-typedef struct PWMChannel {
+#define TOP_MAX 65534
+#define DIV_INT_MIN 1.f
+#define DIV_INT_MAX 255.f
+#define DIV_FRAC_MAX 15.f
+
+typedef struct PWMGPIO {
     uint pin;
     uint slice;
     uint channel;
-} PWMChannel;
+} PWMGPIO;
+
+typedef struct PWMClockDivider {
+    uint8_t int_;
+    uint8_t fract;
+    uint8_t int_only; // Use this just in case the frac part of the divider is not going to be used (less PWM frequency precision)
+} PWMClockDivider;
+
+enum class PWMDir {
+    cw,
+    ccw
+};
 
 class DCMotorDriver {
 public: // Public attributes
-    PWMChannel PWMGPIOs[2];
+    PWMGPIO PWMGPIOs[2];
+    PWMClockDivider PWMDiv;
+    uint16_t pwm_top; // Warp value, this value determines the resolution of the PWM's Duty Cycle
 
     float pwm_freq;
+    bool pwm_phase_correct; // Only set true if phase correct is needed. Remember DC resolution is less in this mode
 public: // Public methods
-    DCMotorDriver(uint pinA1, uint pinA2, float pwm_freq);
-
+    DCMotorDriver(uint pinA1, uint pinA2, float _pwm_freq, bool _pwm_phase_correct=false);
+    /**
+     * @brief Initializes gpios and pwms. Current core's clock frequency shouldn't be changed after calling this method
+     * 
+     */
     void init();
     void setPWMsGPIO();
     void setPWMsFreq();
-    void setPWMsDC();
+    /**
+     * @brief Sets one GPIO ouput to DC% > 0 and the other to DC% = 0 depending on the direction
+     * 
+     * @param dir Direction of Motor. cw: PWMGPIOs[1] at zero% dc, ccw: PWMGPIOs[0] at zero% dc 
+     * @param dc Duty cycle of the GPIO that isn't at zero% dc. Only positive values
+     */
+    void setPWMsDC(PWMDir dir, float dc);
+
+private: // Private attributes
+    float pwm_dc;
+    PWMDir pwm_dir; // Since only one GPIO is outputting pwm at one time, we have pwm_dir and pwm_dc instead of a dc value for each GPIO
 
 private: // Private methods
     void syncPWMsSlices();
+    void calculatePWMsClockTopAndDiv();
+    void calculateClockDiv(uint32_t sys_clock_freq);
 };
